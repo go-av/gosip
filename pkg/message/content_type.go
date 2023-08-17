3da77aa57d5a53e7b032090e5f6a2165fd407bbe
kbdsnail <lp@kuhui.com.cn>
2023-08-17 15:12:20 +0800

    feat: add content type

--- pkg/message/content_type.go
+++ pkg/message/content_type.go
@@ -1,10 +1,9 @@
 package message
 
-var ContentTypeXML = ContentType("Application/MANSCDP+xml")
-
 type ContentType string
 
 const (
 	ContentType__MANSCDP_XML ContentType = "Application/MANSCDP+xml"
 	ContentType__SDP         ContentType = "application/sdp"
+	ContentType__XML         ContentType = "application/xml"
 )
